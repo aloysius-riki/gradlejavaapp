e2520f3d20cf8a1f6883c3a697afe6c6cf4ea80b
Zjukamu Riki <68075271+zjukamu-riki@users.noreply.github.com>
2022-01-05 18:10:04 +0300

    added print colored text external library

--- app/src/main/java/com/zjukamuriki/gradlejavaapp/App.java
+++ app/src/main/java/com/zjukamuriki/gradlejavaapp/App.java
@@ -3,12 +3,21 @@
  */
 package com.zjukamuriki.gradlejavaapp;
 
+import com.diogonunes.jcdp.color.ColoredPrinter;
+import com.diogonunes.jcdp.color.api.Ansi;
+
 public class App {
+
     public String getGreeting() {
         return "Hello World!";
     }
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+
+        ColoredPrinter printer = new ColoredPrinter.Builder(1, false).build();
+
+        printer.print("Hello, colorful world!",
+                Ansi.Attribute.BOLD, Ansi.FColor.BLUE, Ansi.BColor.WHITE);
     }
 }
